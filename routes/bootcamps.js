e494de14ff6d06467625926d0cbf0c3cb61214eb
Abubakar <muhammad.abubakar@virtual-force.com>
2022-03-08 13:44:32 +0500

    Implemented GET API to get all reviews

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 // Include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const {
         getBootcamps,
@@ -22,6 +23,7 @@ const router = express.Router();
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
